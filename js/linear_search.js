1a8709d693f410c63d5d22dd92eaef82fc77b20b
pxl <pxl@pixels.farm>
2023-11-09 10:08:13 +0100

    linear search fix

--- js/linear_search.js
+++ js/linear_search.js
@@ -1,14 +1,14 @@
 //linear search implementation
 
-const x = [5, 3, 67, 24, 6, 22 ,66, 443];
+const x = [5, 3, 67, 24, 6, 22 ,66, 433];
 
 function find(needle, haystack) {
-    let i = -1;
-    while(haystack[i] !== needle) {
+    let i = 0;
+    while(haystack[i] !== needle && i<haystack.length) {
         ++i;
     }
-    return i;
+    return i<haystack.length? i : -1;
 }
 
 
-console.log(find(443, x));
+console.log(find(433, x));
